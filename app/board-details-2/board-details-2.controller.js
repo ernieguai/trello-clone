65bf24bf254964779c7dcb687579ea1fd48bb5f4
Ernie Guaimano <ernie.guaimano@gmail.com>
2016-06-20 00:59:51 -0400

    almost done with card dragging

--- app/board-details-2/board-details-2.controller.js
+++ app/board-details-2/board-details-2.controller.js
@@ -26,56 +26,60 @@ angular.module('trellocloneApp')
     // stage data here to be moved...
     boardDetailsCtrl.models = {
       selected: null,
-      // lists: {"A": [], "B": []}
       cards: boardDetailsCtrl.cards
-    };
-
-    $scope.moveCallback = function($index) {
-      console.log("move $index: ", $index);
     };
 
     $scope.dropCallback = function(event, index, item, external, type, allowedType) {
       console.log("ALL CARDS: ", boardDetailsCtrl.cards);
 
-      // console.log("event: ", event);
-      // console.log("index: ", index);
-      // console.log("item: ", item);
-      // console.log("external: ", external);
-      // console.log("type: ", type);
       // console.log("allowedType: ", allowedType);
       var originCardIndex = boardDetailsCtrl.cards.$indexFor(item.$id);
       console.log("Origin Card Index", originCardIndex);
-      var targetListIndex = event.path[3].id;
-      console.log("Target List Index", targetListIndex);
-
-      // if (external) {
-      //     if (allowedType === 'itemType' && !item.label) return false;
-      //     if (allowedType === 'containerType' && !angular.isArray(item)) return false;
-      // }
-
-
       var originListIndex = item.list;
       console.log("Origin List Index", originListIndex);
 
-      //var droppedList = "";
-      // console.log("droppedList", droppedList);
+      var targetCardIndex = index;
+      console.log("Target Card Index", targetCardIndex);
 
+      var targetListIndex = event.path[3].id;
+      console.log("Target List Index", targetListIndex);
+
+
+      // Move the card into new list
       boardDetailsCtrl.cards[originCardIndex].list = targetListIndex;
 
-      boardDetailsCtrl.cards.$save(originCardIndex);
-      // need to reload the page after this?
+      // update the listNumber (position) for the new card
+      boardDetailsCtrl.cards[originCardIndex].listNumber = targetCardIndex;
 
-      // return item;
+      // increment the list number total for the target list (+1)
+      var originlistId = boardDetailsCtrl.lists.$indexFor(targetListIndex);
+      boardDetailsCtrl.lists[originlistId].cardsInList = boardDetailsCtrl.lists[originlistId].cardsInList + 1;
+      boardDetailsCtrl.lists.$save(originlistId);
+
+      // increment the list number total for the origin list (-1)
+      var listIndex = boardDetailsCtrl.lists.$indexFor(originListIndex);
+      boardDetailsCtrl.lists[listIndex].cardsInList = boardDetailsCtrl.lists[listIndex].cardsInList - 1;
+      boardDetailsCtrl.lists.$save(listIndex);
+
+      function logArrayElements(element, index, array) {
+        if (element.list === targetListIndex) {
+          console.log('a[' + index + '] = ' + element.listNumber);
+          console.log(element.list);
+
+
+          // update list number for the rest of the target list if the list number is >= target number
+          if (element.listNumber >= targetCardIndex) {
+            console.log("updating list number");
+            element.listNumber = element.listNumber + 1;
+          }
+
+        }
+      }
+
+      boardDetailsCtrl.cards.forEach(logArrayElements);
+      boardDetailsCtrl.cards.$save(originCardIndex);
       return true;
    };
-
-   $scope.insertedCallback = function(index, event) {
-     console.log("Inserted callback index: ", index);
-     console.log("Inserted callback event: ", event);
-     console.log("Inserted callback event Path: ", event.path[3].id);
-   };
-
-
 
     boardDetailsCtrl.addNewList = function () {
       boardDetailsCtrl.lists.$add({
@@ -88,15 +92,54 @@ angular.module('trellocloneApp')
     };
 
     boardDetailsCtrl.addNewCard = function (list) {
+
+      var cardsInList = list.cardsInList ? list.cardsInList : 0;
+      console.log("cardsInList: ", cardsInList);
+
+      var listNumber = cardsInList + 1;
+      // upsate list here with $add and new listNumber
+
+      console.log('listNumber', listNumber);
+      console.log('list', list);
+      console.log('list id', list.$id);
+      console.log('index of list: ', boardDetailsCtrl.lists.$indexFor(list.$id));
+
+      var listIndex = boardDetailsCtrl.lists.$indexFor(list.$id);
+      boardDetailsCtrl.lists[listIndex].cardsInList = listNumber;
+      boardDetailsCtrl.lists.$save(listIndex).then(function (success) {
+        console.log("list updated", success);
+      }, function (error) {
+        console.log("list update failed", error);
+      });
+
       boardDetailsCtrl.cards.$add({
         uid: profile.$id,
         list: list.$id,
         title: list.newCard.title,
-        timestamp: Firebase.ServerValue.TIMESTAMP
+        timestamp: Firebase.ServerValue.TIMESTAMP,
+        listNumber: listNumber
       }).then(function () {
         list.newCard = { title: '' };
       });
     };
+
+    // boardsCtrl.starBoard = function(board) {
+    //   console.log("boards", boards);
+    //   console.log("board id", board.$id);
+    //   console.log("index of board: ", boards.$indexFor(board.$id));
+    //   var boardIndex = boards.$indexFor(board.$id);
+    //   if (boardsCtrl.boards[boardIndex].starred === true) {
+    //     boardsCtrl.boards[boardIndex].starred = false;
+    //   } else {
+    //     boardsCtrl.boards[boardIndex].starred = true;
+    //   }
+    //   boardsCtrl.boards.$save(boardIndex);
+    // };
+
+    // upvotesRef.transaction(function (current_value) {
+    //   return (current_value || 0) + 1;
+    // });
+
 
     // Generate initial model
     // for (var i = 1; i <= 3; ++i) {
